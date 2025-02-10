7ea709006eaec9e7589315ae1c68a8d93637a190
SHiTU23 <shi.tu2323@gmail.com>
2025-02-10 17:41:48 +0330

    change variable name, move the text into bracet inside the segmentation function

--- src/text-image-alignment/image_vectorization/VLM/CLIPSeg.py
+++ src/text-image-alignment/image_vectorization/VLM/CLIPSeg.py
@@ -26,8 +26,10 @@ class CLIPSeg:
         SEGMENTATION_THRESHOLD = 0.2 ### threshold for segmentation
         SEGMENT_COLORMAP = [(0, 255, 0), (255, 0, 0), (0, 0, 255)]  # Colors for different objects
         largest_segmented_area = 0
-        self.detected_object = ''
+        self.detected_object_name = ''
         best_box = None
+
+        texts = [texts]
 
         image = Image.open(image_path)
         image_cv = np.array(image)
@@ -60,8 +62,8 @@ class CLIPSeg:
 
             #### the object is detected in the image
             if object_score >= SEGMENTATION_THRESHOLD:
-                self.detected_object = text
-                print(f"Object '{self.detected_object}' detected with max probability {object_score:.2f}")
+                self.detected_object_name = text
+                print(f"Object '{self.detected_object_name}' detected with max probability {object_score:.2f}")
                 
                 ### find contours of the segmented object
                 contours, _ = cv2.findContours(mask_resized, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -78,7 +80,7 @@ class CLIPSeg:
                 if best_box is not None:
                     x, y, w, h = best_box
                     self.bounding_boxed_image = cv2.rectangle(image_cv, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                    self.bounding_boxed_image = cv2.putText(image_cv, f"'{self.detected_object}' probability: {object_score:.2f}", (x-20, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+                    self.bounding_boxed_image = cv2.putText(image_cv, f"'{self.detected_object_name}' probability: {object_score:.2f}", (x - 20, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
                 
                 mask_colored = np.zeros_like(image_cv)
                 mask_colored[:, :, 0] = mask_resized * (SEGMENT_COLORMAP[i][0] / 255)
@@ -87,12 +89,12 @@ class CLIPSeg:
 
                 # Blend mask with image
                 self.segmented_overlay = cv2.addWeighted(self.segmented_overlay, 0.7, mask_colored, 0.3, 0)
-                self.segmented_overlay = cv2.putText(self.segmented_overlay, f"'{self.detected_object}' probability: {object_score:.2f}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+                self.segmented_overlay = cv2.putText(self.segmented_overlay, f"'{self.detected_object_name}' probability: {object_score:.2f}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
             else:
                 print(f"Object '{text}' not detected")
 
     def show_segmented_image(self, segmentations=True, bounding_boxes=True):
-        if self.detected_object != '':
+        if self.detected_object_name != '':
             if segmentations and bounding_boxes:
                 cv2.imshow("CLIPSeg-Segmented Object", self.segmented_overlay)
                 cv2.imshow("CLIPSeg-Bounding Boxes", self.bounding_boxed_image)
@@ -110,9 +112,9 @@ class CLIPSeg:
         images_dir = current_dir + '/images/CLIPSeg/'
 
         
-        if self.detected_object != '':
+        if self.detected_object_name != '':
             self.image_save_counter += 1
-            image_name = self.detected_object + f'_{self.image_save_counter}_T0.2.jpg'
+            image_name = self.detected_object_name + f'_{self.image_save_counter}_T0.2.jpg'
 
             if segmented_image:
                 save_path = images_dir + 'segmented_images/' + image_name
@@ -125,17 +127,17 @@ class CLIPSeg:
 
 if __name__ == "__main__":
 
-    image_segmentor = CLIPSeg()
+    clipseg = CLIPSeg()
 
     current_dir = os.path.dirname(__file__)
     src_dir = os.path.dirname(os.path.dirname(os.path.dirname(current_dir)))
     pic_dir = src_dir + '/simulation/images/'
     image_path = pic_dir + '_image1.jpg'
 
-    texts = ["the cube on the right side of green cube"]
-    image_segmentor.segment_object(image_path, texts)
-    image_segmentor.show_segmented_image(segmentations=True, bounding_boxes=True)
-    image_segmentor.save_image(segmented_image=True, bounding_boxed_image=True)
+    texts = "the blue cube"
+    clipseg.segment_object(image_path, texts)
+    clipseg.show_segmented_image(segmentations=True, bounding_boxes=True)
+    clipseg.save_image(segmented_image=True, bounding_boxed_image=True)
 
 
 
