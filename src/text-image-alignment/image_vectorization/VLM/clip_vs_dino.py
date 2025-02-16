20b95154642f6ab00f18d76d63fefe47ad1d431c
SHiTU23 <shi.tu2323@gmail.com>
2025-02-16 18:13:32 +0330

    comapre CLIPSeg with Grounding DINO

--- src/text-image-alignment/image_vectorization/VLM/clip_vs_dino.py
+++ src/text-image-alignment/image_vectorization/VLM/clip_vs_dino.py
@@ -14,49 +14,58 @@ pic_dir = src_dir + '/simulation/images/'
 
 save_path = current_dir + '/images/compare/'
 
-fig = plt.figure(figsize=(10, 10))
-number_of_images  = 6
-columns = 3
-rows = number_of_images
 
 
 clipseg = CLIPSeg()
 grounding_dino = Grounding_Dino()
 
-text = "blue box"
+text = "red cube"
 
 print(os.listdir(pic_dir))
 
 
-for i, image_name in zip(range(1, rows*columns +1, columns),  os.listdir(pic_dir)[:number_of_images]):
+for i, image_name in enumerate(os.listdir(pic_dir)):
     image_path = pic_dir + image_name
     print(image_path)
 
+    clip_process_time_start = time.time()
     clip_features, clip_seg_img, clip_bb_img = clipseg.segment_object(image_path, text)
+    clip_process_time_end = time.time()
+
+    dino_process_time_start = time.time()
     dino_features, dino_bb_img = grounding_dino.detect_object(image_path, text)
+    dino_process_time_end = time.time()
 
     print(f"CLIPSeg features: {clip_features}")
     print(f"Grounding DINO features: {dino_features}")
+
 
     clip_seg_img = cv2.cvtColor(clip_seg_img, cv2.COLOR_BGR2RGB)
     clip_bb_img = cv2.cvtColor(clip_bb_img, cv2.COLOR_BGR2RGB)
     dino_bb_img = cv2.cvtColor(dino_bb_img, cv2.COLOR_BGR2RGB)
 
-    plt.subplot(rows, columns, i)
-    plt.imshow(clip_seg_img)
-    plt.axis('off')
-    plt.title("CLIPSeg-segmenet objects")
 
-    plt.subplot(rows, columns, i+1)
-    plt.imshow(clip_bb_img)
-    plt.axis('off')
-    plt.title("CLIPSeg-box objects")
+    fig, axes = plt.subplots(1, 3, figsize=(12, 4), gridspec_kw={'width_ratios': [1, 1, 1]})  # Ensure equal widths
+    fig.suptitle(f"query: '{text}'")
 
-    plt.subplot(rows, columns, i+2)
-    plt.imshow(dino_bb_img)
-    plt.axis('off')
-    plt.title("Grounding DINO-box objects")
+    axes[0].imshow(clip_seg_img)
+    axes[0].set_title("CLIPSeg-segmenet objects")
 
-plt.savefig(save_path + 'compare.png')
-plt.show()
+    axes[1].imshow(clip_bb_img)
+    axes[1].set_title("CLIPSeg-box objects, pt: {:.2f}s".format(clip_process_time_end - clip_process_time_start))
+
+    axes[2].imshow(dino_bb_img)
+    axes[2].set_title("Grounding DINO-box objects, pt: {:.2f}s".format(dino_process_time_end - dino_process_time_start))
+
+    # Remove axes for clean display
+    for ax in axes:
+        ax.axis("off")
+
+
+    plt.tight_layout()
+    plt.savefig(save_path + f'{text}_compare{i}.jpg')
+    
+    print("image saved")
+    # plt.show()
+
 
