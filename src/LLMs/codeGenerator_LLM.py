cb19ecedae32a160b8e443f7a5279967f6435774
SHiTU23 <shi.tu2323@gmail.com>
2025-03-03 14:42:12 +0330

    Refactor: create a class for the llm to answer queries
    + add chat-history: saving a summary of the answers and give it to LLM every time.
    + the `generate_answer` method has the option to use RAG or not.

--- src/LLMs/codeGenerator_LLM.py
+++ src/LLMs/codeGenerator_LLM.py
@@ -9,101 +9,150 @@ import json
 
 
 
+class second_layer_LLM:
+    def __init__(self):
+        self._AZURE_OPENAI_ENDPOINT = "https://aihubthesiswes8755517667.openai.azure.com/"
+        self._AZURE_CHAT_MODELNAME = "gpt-4o-codeGenerator"
+        self._AZURE_EMBEDDING_MODEL = "text-embedding-3-large"
 
+        self._AZURE_SEARCH_SERVICE = "https://thesis-west-aisearch.search.windows.net"
+        self._AZURE_SEARCH_ADMIN_KEY = "RyHN2fSd1HrLqlwpfUuTbVYwmTO4W9rR3OxO9FZko6AzSeDsPllI"
 
+        _index_name = "docs-index"
 
-AZURE_OPENAI_ENDPOINT = "https://aihubthesiswes8755517667.openai.azure.com/"
-AZURE_CHAT_MODELNAME = "gpt-4o-codeGenerator"
-AZURE_EMBEDDING_MODEL = "text-embedding-3-large"
+        _credential = AzureKeyCredential(self._AZURE_SEARCH_ADMIN_KEY)
+        self._openai_client = AzureOpenAI(
+                                            api_version="2024-06-01",
+                                            azure_endpoint=self._AZURE_OPENAI_ENDPOINT,
+                                         )
 
-AZURE_SEARCH_SERVICE = "https://thesis-west-aisearch.search.windows.net"
-AZURE_SEARCH_ADMIN_KEY = "RyHN2fSd1HrLqlwpfUuTbVYwmTO4W9rR3OxO9FZko6AzSeDsPllI"
+        self._search_client = SearchClient(
+                                            endpoint=self._AZURE_SEARCH_SERVICE,
+                                            index_name=_index_name,
+                                            credential=_credential
+                                          )
 
-index_name = "rag-robotics-docs-index"
+        self._chat_history = []
+    def get_embeddings_vector(self, text):
+        response = self._openai_client.embeddings.create(
+                                                            input=text,
+                                                            model=self._AZURE_EMBEDDING_MODEL,
+                                                            dimensions=1024,
+                                                        )
+        embedding = response.data[0].embedding
+        return embedding
 
-credential = AzureKeyCredential(AZURE_SEARCH_ADMIN_KEY)
+    def generate_answer(self, query, RAG_inUse=True):
+        if RAG_inUse:
+            # Provide instructions to the model
+            _GROUNDED_PROMPT="""
+            You are an AI assistant for a UR5 robot in ROS and Gazebo. Your task is to generate action plans and Python code for robotic queries. If the query is not robotics-related, provide a brief (2-3 sentence) reliable answer.
 
-def get_embeddings_vector(text):
+            - Use the provided sources to improve accuracy.
+            - Extract only the necessary parts from sources, not entire methods.
+            - Refer to the chat history for context.
+            - Use bullet points for multi-point answers.
+            - If unsure, state that you lack enough information.
+            - Cite sources when using them.
 
-    response = openai_client.embeddings.create(
-        input=text,
-        model=AZURE_EMBEDDING_MODEL,
-        dimensions=1024,
-    )
+            At the end, include a concise summary labeled **"history:"** for future reference.
 
-    embedding = response.data[0].embedding
+            Query: {query}
 
-    return embedding
+            Chat history:
+            {history}
 
+            Sources:
+            {sources}
+            """
 
-endpoint = os.getenv("ENDPOINT_URL", "https://aihubthesiswes8755517667.openai.azure.com/")  
-deployment = os.getenv("DEPLOYMENT_NAME", "gpt-4o-codeGenerator")  
-subscription_key = os.getenv("AZURE_OPENAI_API_KEY", "16e5XT0Seh7kF6knUDWkQsLodd3otgpNR3uJuCOkyXJlVK9181MAJQQJ99BBACfhMk5XJ3w3AAAAACOGguO1")  
-# token_provider = get_bearer_token_provider(credential, "https://cognitiveservices.azure.com/.default")
-openai_client = AzureOpenAI(
-     api_version="2024-06-01",
-     azure_endpoint=AZURE_OPENAI_ENDPOINT,
-    #  azure_ad_token_provider=token_provider
- )
+            # You are an AI assistant for a UR5 robot in ROS and Gezebo that generates action plans and then Python codes for it to perform the robotics task requested in the query when the query is a robotic task. otherwise, provide 2-3 sentences short reliable answers to the quetions that are not a robotic_task.
+            # Get help in answer the query using the sources provided below and make your final response more correct. 
+            # Do not use methods of the class from the provided resources in the answer directly, instead use the necessary parts in it for performimng the whole task.
+            # Also, you have the chat history to refer to the previous queries and answers.
+            # Use bullet points if the answer has multiple points.
+            # If you don't know the answer, say you don't have enough information.
+            # If answering using the sources provided below, cite your source when you answer the question.
+            # At the end give a very short summary to be used as a history in the chatm label "history:".
+            # Query: {query}
+            # \n
+            # Chat history:
+            # {history}   
+            # \n
+            # Do not change the format of the references below.
+            # Sources:\n{sources}
+            
 
-search_client = SearchClient(
-     endpoint=AZURE_SEARCH_SERVICE,
-     index_name=index_name,
-     credential=credential
- )
+            # Provide the search query. 
+            # The vector query finds 3 nearest neighbor matches in the search index
+            # query="pick the green cube."
+            _embedding = self.get_embeddings_vector(query)
+            _vector_query = VectorizedQuery(vector=_embedding, k_nearest_neighbors=3, fields="text_vector")
 
-# Provide instructions to the model
-GROUNDED_PROMPT="""
-You are an AI assistant for a UR5 robot in ROS and Gezebo that generates action plans and then Python codes for it to perform the robotics task requested in the query when the query is a robotic task. otherwise, provide 2-3 sentences short reliable answers to the quetions that are not a robotic_task.
-Get help in answer the query using the sources provided below and make your final response more correct.
-Use bullets if the answer has multiple points.
-If you don't know the answer, say you don't have enough information.
-If answering using the sources provided below, cite your source when you answer the question
-Query: {query}
-\n
-Do not change the format of the references below.
-Sources:\n{sources}
-"""
+            # Set up the search results and the chat thread.
+            # Retrieve the selected fields from the search index related to the question.
+            # Search results are limited to the top 5 matches. Limiting top can help you stay under LLM quotas.
+            search_results = self._search_client.search(
+                                                        search_text=query,
+                                                        vector_queries= [_vector_query],
+                                                        select=["title", "chunk"],
+                                                        top=5,
+                                                        )
 
-# Provide the search query. 
-# The vector query finds 3 nearest neighbor matches in the search index
+            # Use a unique separator to make the sources distinct, such as === 
+            _sources_formatted = "=================\n".join([f'TITLE: {document["title"]}, CONTENT: {document["chunk"]}' for document in search_results])
+            _content = _GROUNDED_PROMPT.format(query=query, history=self._chat_history, sources=_sources_formatted)
 
-query="do I need to attach the object the robot wants to pick to the gripper?"
-# query="pick the green cube."
-embedding = get_embeddings_vector(query)
+        else:
+            _GROUNDED_PROMPT = """
+            You are an AI assistant for a UR5 robot in ROS and Gezebo that generates action plans and then Python codes for it to perform the robotics task requested in the query when the query is a robotic task. otherwise, provide 2-3 sentences short reliable answers to the quetions that are not a robotic_task.
+            Use bullet points if the answer has multiple points.
+            If you don't know the answer, say you don't have enough information.
+            At the end give a very short summary to be used as a history in the chatm label "history:".
+            Query: {query}
+            """
+            _content = _GROUNDED_PROMPT.format(query=query)
 
-vector_query = VectorizedQuery(vector=embedding, k_nearest_neighbors=3, fields="text_vector")
+        response = self._openai_client.chat.completions.create(
+                                                                messages=[
+                                                                    {
+                                                                        "role": "user",
+                                                                        "content": _content
+                                                                    }
+                                                                ],
+                                                                model=self._AZURE_CHAT_MODELNAME,
+                                                                max_tokens=5000,  
+                                                                temperature=0.5,  
+                                                                top_p=0.95,  
+                                                                frequency_penalty=0,  
+                                                                presence_penalty=0,
+                                                                stop=None,  
+                                                                stream=False
+                                                            )
+        llm_response = response.choices[0].message.content
+        summary = self.extract_summary(llm_response)
+        history = {
+                    "query": query,
+                    "summary": summary
+                 }
+        self._chat_history.append(history)
 
-# Set up the search results and the chat thread.
-# Retrieve the selected fields from the search index related to the question.
-# Search results are limited to the top 5 matches. Limiting top can help you stay under LLM quotas.
-search_results = search_client.search(
-    search_text=query,
-    vector_queries= [vector_query],
-    select=["title", "chunk"],
-    top=5,
-)
+        
+        return llm_response
+    
+    def extract_summary(self, text):
+        keyword = "history"
+        index = text.lower().find(keyword) 
+        
+        if index != -1:  # If "summary" is found
+            return text[index:] 
+        else:
+            return None 
 
-# Newlines could be in the OCR'd content or in PDFs, as is the case for the sample PDFs used for this tutorial.
-# Use a unique separator to make the sources distinct. 
-# We chose repeated equal signs (=) followed by a newline because it's unlikely the source documents contain this sequence.
-sources_formatted = "=================\n".join([f'TITLE: {document["title"]}, CONTENT: {document["chunk"]}' for document in search_results])
-
-response = openai_client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": GROUNDED_PROMPT.format(query=query, sources=sources_formatted)
-        }
-    ],
-    model=AZURE_CHAT_MODELNAME,
-    max_tokens=5000,  
-    temperature=0.7,  
-    top_p=0.95,  
-    frequency_penalty=0,  
-    presence_penalty=0,
-    stop=None,  
-    stream=False
-)
-
-print(response.choices[0].message.content)
+if __name__ == "__main__":
+    llm = second_layer_LLM()
+    while True:
+        query = input("Enter query: ")
+        response = llm.generate_answer(query, RAG_inUse=True)
+        print(response)
+        print("=====================================")
